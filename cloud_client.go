da57e51096364ea93ca39f5bb37e25496d8cd01a
zengningzhong <zengningzhong@bitmart.com>
2020-07-10 11:03:36 +0800

    add test data into cloud_client_test

--- cloud_client.go
+++ cloud_client.go
@@ -23,25 +23,6 @@ type ApiMessage struct {
 
 
 
-
-func GetDefaultConfig() *Config {
-	var config Config
-
-	config.Url = "http://api-cloud.bitmartdev.com"
-	config.ApiKey = "80618e45710812162b04892c7ee5ead4a3cc3e56"
-	config.SecretKey = "6c6c98544461bbe71db2bca4c6d7fd0021e0ba9efc215f9c6ad41852df9d9df9"
-	config.Memo = "test001"
-	config.TimeoutSecond = 30
-	config.IsPrint = true
-	return &config
-}
-
-func NewTestClient() *CloudClient {
-	client := NewClient(*GetDefaultConfig())
-	return client
-}
-
-
 /*
  Get a http client
 */
