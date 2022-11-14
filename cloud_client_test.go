974d03b61b5ce700b48ae30ad9da6959224eb503
ricci <ricci@ponfty.com>
2022-11-14 17:22:35 +0800

    remove test data

--- cloud_client_test.go
+++ cloud_client_test.go
@@ -5,9 +5,9 @@ func GetDefaultConfig() *Config {
 
 	config.Url = API_URL_PRO
 	config.WsUrl = WS_URL
-	config.ApiKey = "80618e45710812162b04892c7ee5ead4a3cc3e56"
-	config.SecretKey = "6c6c98544461bbe71db2bca4c6d7fd0021e0ba9efc215f9c6ad41852df9d9df9"
-	config.Memo = "test001"
+	config.ApiKey = "Your API KEY"
+	config.SecretKey = "Your Secret KEY"
+	config.Memo = "Your Memo"
 	config.TimeoutSecond = 30
 	config.IsPrint = true
 	return &config
@@ -28,9 +28,9 @@ func GetWSPrivateConfig() *Config {
 
 	config.Url = API_URL_PRO
 	config.WsUrl = WS_URL_USER
-	config.ApiKey = "80618e45710812162b04892c7ee5ead4a3cc3e56"
-	config.SecretKey = "6c6c98544461bbe71db2bca4c6d7fd0021e0ba9efc215f9c6ad41852df9d9df9"
-	config.Memo = "test001"
+	config.ApiKey = "Your API KEY"
+	config.SecretKey = "Your Secret KEY"
+	config.Memo = "Your Memo"
 	config.TimeoutSecond = 30
 	config.IsPrint = true
 	return &config
